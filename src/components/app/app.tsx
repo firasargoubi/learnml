f50f54b62c51c2ff113e91867da70567965783fd
Alexandre Falardeau <alexandre.h.falardeau@gmail.com>
2020-10-04 13:38:17 -0400

    feat: translation setup i18next

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import logo from '../../images/logo.svg';
+import { useTranslation } from 'react-i18next'
 import './app.scss';
 
 function App() {
+  const { t } = useTranslation()
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/components/app/app.tsx</code> and save to reload!
+          {t('App.edit0')} <code>src/components/app/app.tsx</code> {t('App.edit1')}
         </p>
         <a
           className="App-link"
@@ -16,7 +18,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          {t('App.learnReact')}
         </a>
       </header>
     </div>
