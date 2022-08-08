88369a11552bd40a74cbd79301ddadee70323839
Jean-François Senécal <jeanjeansenecal@gmail.com>
2022-08-08 19:27:43 -0400

    feat(form): added link to form

--- src/components/landing-hero-page/landing-hero-page.tsx
+++ src/components/landing-hero-page/landing-hero-page.tsx
@@ -14,20 +14,18 @@ const LandingHeroPage: React.FC = () => {
     setShowParticles(window.innerWidth <= 768);
   };
 
-  useEffect( () => {
+  useEffect(() => {
     window.addEventListener('resize', handleResize);
     handleResize();
     return () => window.removeEventListener('resize', handleResize);
   }, []);
-  
-  useEffect( () => {
-    if(showParticles) {
+
+  useEffect(() => {
+    if (showParticles) {
       if (window.innerWidth < 300) {
         setRatio(30);
-
       } else if (window.innerWidth < 400) {
         setRatio(60);
-        
       } else {
         setRatio(100);
       }
@@ -36,35 +34,75 @@ const LandingHeroPage: React.FC = () => {
 
   return (
     <React.Fragment>
-      <div className={showParticles ? 'landing-hero-page' : 'landing-hero-page bg-color-hack'}>
-        <Row sm="1" md="2" className="content">
-          <Col sm={{ order: 'last' }} md={{ order: 'first' }} className="text-container">
-              <h1 className="title">{t('LandingHeroPage.firstTitle')} : {t('LandingHeroPage.secondTitle')}</h1>
-              <p className="subtitle">{t('LandingHeroPage.subtitle')}</p>
-              <p className="subtitle"> {t('LandingHeroPage.when')} </p>
-              <button className="register">
-                <a href="" target="_blank" rel="noopener noreferrer">  {t('LandingHeroPage.register')}  </a>
-              </button>
+      <div
+        className={
+          showParticles
+            ? 'landing-hero-page'
+            : 'landing-hero-page bg-color-hack'
+        }
+      >
+        <Row sm='1' md='2' className='content'>
+          <Col
+            sm={{ order: 'last' }}
+            md={{ order: 'first' }}
+            className='text-container'
+          >
+            <h1 className='title'>
+              {t('LandingHeroPage.firstTitle')} :{' '}
+              {t('LandingHeroPage.secondTitle')}
+            </h1>
+            <p className='subtitle'>{t('LandingHeroPage.subtitle')}</p>
+            <p className='subtitle'> {t('LandingHeroPage.when')} </p>
+            <button className='register'>
+              <a
+                href='https://docs.google.com/forms/d/e/1FAIpQLSd5kurd7qe4_XhSdkrvRPuor5J2RiE1ERFX3MwDV9pTpeTpQg/viewform?usp=sf_link'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                {' '}
+                {t('LandingHeroPage.register')}{' '}
+              </a>
+            </button>
           </Col>
-          <Col sm={{ order: 'first' }} md={{ order: 'last' }} className="neural-net-image"/>
+          <Col
+            sm={{ order: 'first' }}
+            md={{ order: 'last' }}
+            className='neural-net-image'
+          />
         </Row>
-        <div className="mobile-content">
-          <h1 className="title">{t('LandingHeroPage.firstTitle')}</h1>
-          <h1 className="title">{t('LandingHeroPage.secondTitle')}</h1>
-          <p className="subtitle">{t('LandingHeroPage.subtitle')}</p>
-          <p className="subtitle"> {t('LandingHeroPage.when')} </p>
-          <button className="register">
-            <a href="" target="_blank" rel="noopener noreferrer">  {t('LandingHeroPage.register')}  </a>
+        <div className='mobile-content'>
+          <h1 className='title'>{t('LandingHeroPage.firstTitle')}</h1>
+          <h1 className='title'>{t('LandingHeroPage.secondTitle')}</h1>
+          <p className='subtitle'>{t('LandingHeroPage.subtitle')}</p>
+          <p className='subtitle'> {t('LandingHeroPage.when')} </p>
+          <button className='register'>
+            <a
+              href='https://docs.google.com/forms/d/1-8aDgNvrfHa7oHooxRSXH2axk-m9wxtXxBG4XXXpbFk/prefill'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              {' '}
+              {t('LandingHeroPage.register')}{' '}
+            </a>
           </button>
         </div>
-        <Link className="a scroll-down" to="about" smooth={true} duration={1000} />
-      </div> 
+        <Link
+          className='a scroll-down'
+          to='about'
+          smooth={true}
+          duration={1000}
+        />
+      </div>
 
-      {showParticles && 
-         <ParticlesBg color="#04cfda" num={ratio} type="cobweb" bg={showParticles}  /> }
-
+      {showParticles && (
+        <ParticlesBg
+          color='#04cfda'
+          num={ratio}
+          type='cobweb'
+          bg={showParticles}
+        />
+      )}
     </React.Fragment>
-    
   );
 };
 
