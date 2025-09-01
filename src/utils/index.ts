3ec19416ab619c8c1925ba8e288fa417d417be85
Firas Argoubi <99605613+firasargoubi@users.noreply.github.com>
2025-09-01 17:24:04 -0400

    Refine range utility to yield indices

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,13 +1,13 @@
 export const range = (n: number) => {
-    return [...Array(n)];
+    return Array.from({ length: n }, (_, i) => i);
 };
 
 export const batch = <T>(arr: T[], n: number) => {
     if (arr.length === 0) {
         return [];
     } else {
-        return range(Math.ceil((arr.length) / n)).map((_, i) =>
-            range(Math.min(n, arr.length - i * n)).map((__, j) => arr[i * n + j]),
+        return range(Math.ceil(arr.length / n)).map((i) =>
+            range(Math.min(n, arr.length - i * n)).map((j) => arr[i * n + j]),
         );
     }
 };
