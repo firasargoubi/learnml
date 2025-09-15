9c9cd42d2a5fcc86acf8df96cdb2b9a37a67c8bc
Firas Argoubi <firas.argoubi@polymtl.ca>
2025-09-14 22:30:22 -0400

    modified hours for events

--- src/components/schedule/schedule-data.ts
+++ src/components/schedule/schedule-data.ts
@@ -3,49 +3,49 @@ import { Event } from './event-interface';
 export const EVENTS: Event[][] = [
   [
     {
-      time: '09:00 - 10:00',
+      time: '08:45 - 09:30',
       descriptioni18nKey: 'Event[0][0].description',
     },
     {
-      time: '10:00 - 10:30',
+      time: '9:30 - 10:00',
       descriptioni18nKey: 'Event[0][1].description',
     },
     {
-      time: '10:30 - 12:00',
+      time: '10:00 - 13:00',
       descriptioni18nKey: 'Event[0][2].description',
     },
     {
-      time: '12:00 - 13:00',
+      time: '13:00 - 14:00',
       descriptioni18nKey: 'Event[0][3].description',
     },
     {
-      time: '13:00 - 15:00',
+      time: '14:00 - 17:00',
       descriptioni18nKey: 'Event[0][4].description',
     },
   ],
   [
     {
-      time: '09:00 - 10:00',
+      time: '08:45 - 09:30',
       descriptioni18nKey: 'Event[1][0].description',
     },
     {
-      time: '10:00 - 10:30',
+      time: '9:30 - 10:00',
       descriptioni18nKey: 'Event[1][1].description',
     },
     {
-      time: '10:30 - 12:00',
+      time: '10:00 - 13:00',
       descriptioni18nKey: 'Event[1][2].description',
     },
     {
-      time: '12:00 - 13:00',
+      time: '13:00 - 14:00',
       descriptioni18nKey: 'Event[1][3].description',
     },
     {
-      time: '13:00 - 15:00',
+      time: '14:00 - 17:00',
       descriptioni18nKey: 'Event[1][4].description',
     },
     {
-      time: '15:00 - 16:00',
+      time: '17:00 - 17:30',
       descriptioni18nKey: 'Event[1][5].description',
     },
   ],
